dfa57bdd833f36d10dea59878b1d809eeaba7da5
shino-prasad <75426642+shino-prasad@users.noreply.github.com>
2022-09-05 12:01:10 +0530

    Migrating Project from local to git

---
+++ src/main/java/com/pluralsight/blog/data/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.pluralsight.blog.data;
+
+import com.pluralsight.blog.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {}
---
+++ src/main/java/com/pluralsight/blog/model/Category.java
@@ -0,0 +1,40 @@
+package com.pluralsight.blog.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @OneToMany
+    List<Post> posts;
+
+    public Category() {
+        super();
+        posts = new ArrayList<>();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void addPost(Post post) {
+        posts.add(post);
+    }
+}
